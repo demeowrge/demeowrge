5cdd5bd1f9c0901442fb29703aad134480ca2039
Dia6lo <Dia6lo@mail.ru>
2015-04-24 21:09:59 +1100

    LevelManager Base

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public static class LevelManager {
+
+    public static void NextLevel()
+    {
+        throw new NotImplementedException();    
+    }
+    public static void RestartLevel()
+    {
+        throw new NotImplementedException();
+    }
+    public static void Save()
+    {
+        throw new NotImplementedException();
+    }
+    public static void Load()
+    {
+        throw new NotImplementedException();
+    }
+    public static void NewGame()
+    {
+        throw new NotImplementedException();
+    }
+    public static void Exit()
+    {
+        Environment.Exit(0);
+    }
+}
