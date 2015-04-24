a754c0e6408f836c8871d5cccd10998e0391e44a
Mehael <mehael@mail.ru>
2015-04-24 22:13:43 +1000

    CatAnimationMachine

---
+++ Assets/Animations/CatAnimationsController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatAnimationsController : MonoBehaviour {
+    public float idleTimeToSleep;
+    private Vector3 lastPosition;
+    private float idleTime;
+    private bool wasMoving;
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        lastPosition = transform.position;
+        wasMoving = false;
+    }
+
+    void Update () {
+        var vect = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += vect * 1f * Time.deltaTime;
+
+        var positionDelta = transform.position - lastPosition;
+
+        if (positionDelta == Vector3.zero)
+        {
+            if (wasMoving)
+            {
+                animator.SetTrigger("Stop");
+                wasMoving = false;
+            }
+            idleTime = idleTime + Time.deltaTime;
+            if (idleTime > idleTimeToSleep)
+                animator.SetBool("isSleep", true);
+        }
+        else
+        {
+            idleTime = 0;
+            animator.SetBool("isSleep", false);
+            wasMoving = true;
+
+            if (positionDelta.x == 0)
+                animator.SetFloat("HorisontalSpeed", -1);
+            else
+                animator.SetFloat("HorisontalSpeed", positionDelta.x);
+        }
+
+        lastPosition = transform.position;
+	}
+
+    public void Death()
+    {
+        animator.SetTrigger("Death");
+    }
+}
---
+++ Assets/Scripts/Field.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Field : MonoBehaviour {
+	public Transform prefabFieldSprite;
+	public int fieldWidth;
+	public int fieldHeight;
+	public int ditchYPosition;
+
+	public int pxTileWidth;
+	public int pxTileHeight;
+
+	// Draw field
+	void Start () {
+		Transform newTile;
+
+		float TileWidth = pxTileWidth * 0.01f; // pixels to space
+		float TileHeight = pxTileHeight * 0.01f;
+
+		for (int i = 0; i < fieldWidth; i++){ //Copy-Paste
+			for (int j = 0; j < fieldHeight; j++){ 
+				if(j!=ditchYPosition){
+					newTile = Instantiate(prefabFieldSprite, new Vector3(transform.position.x + i * TileWidth,
+				     	transform.position.y + j * TileHeight,0), transform.rotation) as Transform;
+					newTile.parent = transform;
+				}
+			}
+		}
+	}
+
+}
