2af1504fa4da5deeae2f48c96688022306b2d3a5
Dia6lo <Dia6lo@mail.ru>
2015-04-25 01:04:17 +1100

    Add Spell base

---
+++ Assets/Scripts/Behaviours/MouseFollow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseFollow : MonoBehaviour {
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+
+	public GameObject spell;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        Instantiate(spell);
+	    }
+	}
+}
