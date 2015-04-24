35217cbdee071f1ee573be36756a29784988c625
Mehael <mehael@mail.ru>
2015-04-24 20:52:02 +1000

    Свистелки в главном меню

---
+++ Assets/Scripts/Utils/DestroyAfterDelay.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterDelay : MonoBehaviour
+{
+    public float lifetime;
+
+	void Awake () {
+        Destroy(gameObject, lifetime);
+	}
+	
+}
---
+++ Assets/Scripts/mainMenuCatsSpawner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenuCatsSpawner : MonoBehaviour {
+	public int catsPauseChance;
+    public GameObject menuCatsPrefab;
+	
+	void Update () {
+        if (Random.Range(0, catsPauseChance) == 0)
+        {
+              Instantiate(menuCatsPrefab, transform.position, transform.rotation);
+        };
+	}
+}
