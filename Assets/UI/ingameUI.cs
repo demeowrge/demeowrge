4ce802aec376976805665e8afabccd2edb70d384
Dia6lo <Dia6lo@mail.ru>
2015-04-25 10:10:48 +1100

    Remove test script

--- Assets/UI/ingameUI.cs
+++ Assets/UI/ingameUI.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class ingameUI : MonoBehaviour {
     void Awake()
     {
-        DontDestroyOnLoad(transform.gameObject);
+        //DontDestroyOnLoad(transform.gameObject);
     }
 
     public void bnProphetClick()
