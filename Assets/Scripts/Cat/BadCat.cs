a7f581241dec694fdf26ec5d8216aad0cb2e6dc2
Roman Titov <titovroman@gmail.com>
2015-04-25 22:10:03 +1000

    Bad cat... Bad cat!.. BAD CAT!!1

---
+++ Assets/Scripts/Cat/BadCat.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+
+public class BadCat : MonoBehaviour
+{
+    public float Speed;
+
+    protected Collider2D chaseCollider;
+    protected WorstEffectEver worstEffectEver;
+
+    void Start()
+    {
+        worstEffectEver = transform.GetComponentInChildren<WorstEffectEver>();
+
+        chaseCollider = gameObject.GetComponent<Collider2D>();
+        chaseCollider.enabled = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        Cat cat = obj.GetComponent<Cat>();
+        if (cat != null)
+        {
+            chaseCollider.enabled = false;
+            StartCoroutine(ChaseCoroutine(cat));
+        }
+    }
+
+    protected IEnumerator ChaseCoroutine(Cat cat)
+    {
+        while (worstEffectEver.chased == false && Vector3.Distance(transform.position, cat.transform.position) > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, cat.transform.position, Speed * Time.deltaTime);
+            yield return null;
+        }
+
+        worstEffectEver.chased = false;
+    }
+}
---
+++ Assets/Scripts/Effects/WorstEffectEver.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorstEffectEver : AbsCatEffect
+{
+    public bool chased = false;
+    public GameObject BadCat;
+
+    protected override void CatEffect(Cat cat)
+    {
+        chased = true;
+        Vector3 position = cat.transform.position;
+        Destroy(cat.gameObject);
+        Instantiate(BadCat, position, Quaternion.identity);
+    }
+}
