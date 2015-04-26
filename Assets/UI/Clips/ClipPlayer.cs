eba30c86aa53be4e2f8ad5a5afb70a09fb2aedee
Roman Titov <titovroman@gmail.com>
2015-04-26 11:21:01 +1000

    Shitload of code.

--- Assets/UI/Clips/ClipPlayer.cs
+++ Assets/UI/Clips/ClipPlayer.cs
@@ -2,38 +2,51 @@
 
 public class ClipPlayer : MonoBehaviour
 {
-    public bool isLastClip = false;
     public GameObject[] slides;
+    public bool isLastClip = false;
 
     private bool isEnded;
     private int SlideNumber;
 
-	void Start ()
-	{
-	    SlideNumber = -1;
+    void Start()
+    {
+        SlideNumber = -1;
         NextSlide();
-	}
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            NextSlide();
+        }
+    }
 
     void NextSlide()
     {
         if (isEnded) return;
+
         SlideNumber++;
 
         if (SlideNumber >= slides.GetLength(0))
         {
             isEnded = true;
             if (!isLastClip)
+            {
+                Debug.Log("Yay!");
                 LevelManager.NextLevel();
+            }
             else
+            {
                 LevelManager.MainMenu();
-        } else
+            }
+        }
+        else
+        {
             for (int i = 0; i < slides.GetLength(0); i++)
+            {
                 slides[i].SetActive(i == SlideNumber);
+            }
+        }
     }
-
-	void FixedUpdate()
-	{
-	    if (Input.GetMouseButtonDown(0))
-	        NextSlide();
-	}
 }
