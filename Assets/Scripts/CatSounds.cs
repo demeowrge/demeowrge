e9bcd2bfca3d1183c4fbcd5f30264a3cfc31f2cb
Mehael <mehael@mail.ru>
2015-04-26 11:38:27 +1000

    Все звуки проигрываются

---
+++ Assets/Scripts/CatSounds.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatSounds : MonoBehaviour {
+    public AudioClip mur_1;
+    public AudioClip burr_1;
+    public int silenceChance;
+
+    private AudioSource auPlayer;
+	void Start () {
+        auPlayer = gameObject.GetComponent<AudioSource>();
+	}
+
+    public void playBurr()
+    {
+        auPlayer.clip = burr_1;
+        auPlayer.Play(); 
+    }
+
+    public void playMurr()
+    {
+        playSound(mur_1);
+    }
+
+    void playSound(AudioClip clip)
+    {
+        if (Random.Range(0, silenceChance) == 0)
+        {
+            auPlayer.clip = clip;
+            auPlayer.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/DontDestroyBetweenScenes.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDestroyBetweenScenes : MonoBehaviour {
+	void Awake () {
+        DontDestroyOnLoad(gameObject);
+	}
+}
